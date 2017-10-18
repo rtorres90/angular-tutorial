afc530d745013969b90cd126be76d0da9cd51969
Roberto Torres <roberto.torres.dev@gmail.com>
2017-10-18 05:47:23 -0700

    bindings and pipes :D

--- start/src/app/app.module.ts
+++ start/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import {ProductListComponent} from "./products/product-list.component";
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
